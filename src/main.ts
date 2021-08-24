2690b1aa7f1a2027cdd7301b406ae512e21b674a
Stharley Maxwell <stharleymax@gmail.com>
2021-08-24 11:08:23 -0300

    add lib moment-timezone

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import * as momentTimeZone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function(): any {
+    return momentTimeZone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
 
   app.useGlobalPipes(
     new ValidationPipe({
